071167afb668db2ff49bde0f7f96cd928c4583c2
salsabilmislat <salsabilmislat196@gmail.com>
2021-12-14 17:54:59 +0200

    done2

--- languages/country/urls.py
+++
@@ -1,8 +0,0 @@
-from django.urls import path
-from .views import CountryList, CountryDetail
-
-urlpatterns = [
-    path('', CountryList.as_view(), name = 'country_list'),
-    path('<int:pk>/',CountryDetail.as_view(), name = 'country_detail')
-
-]
